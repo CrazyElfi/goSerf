52ef90747344c555929da28721bed2a04fcc12f8
CrazyElfi <al.gaffarova.26@gmail.com>
2019-10-27 23:34:24 +0300

    added new blocks

--- app/js/main.js
+++ app/js/main.js
@@ -4,13 +4,29 @@ $(function(){
         fade: true,
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg">',
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg">',
-        asNavFor: '.slider-dots'
+        asNavFor: '.slider-dotshead'
     });
 
-    $('.slider-dots').slick({
+    $('.slider-dotshead').slick({
         slidesToShow: 4,
         slidesToScroll: 1,
         asNavFor: '.header__slider'
+    });
+
+    $('.surf-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg">',
+        asNavFor: '.slider-map'
+    });
+
+    $('.slider-map').slick({
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        arrows: false,
+        asNavFor: '.surf-slider',
+        focusOnSelect:true
     });
   
 });
--- gulpfile.js
+++ gulpfile.js
@@ -75,8 +75,8 @@ gulp.task('export', function(){
   let BuildFonts = gulp.src('app/fonts/**/*.*')
     .pipe(gulp.dest('dist/fonts'))
 
-  let BuildImg = gulp.src('app/images/**/*.*')
-    .pipe(gulp.dest('dist/images'))
+  let BuildImg = gulp.src('app/img/**/*.*')
+    .pipe(gulp.dest('dist/img'))
 });
 
 gulp.task('watch', function() {
