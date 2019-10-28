e3e0d76150a725350ec34064f7e4ce482d0e1787
CrazyElfi <al.gaffarova.26@gmail.com>
2019-10-28 16:57:30 +0300

    added new block

--- app/js/main.js
+++ app/js/main.js
@@ -28,5 +28,12 @@ $(function(){
         asNavFor: '.surf-slider',
         focusOnSelect:true
     });
+
+    $('.travel__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg">',
+    });
   
 });
